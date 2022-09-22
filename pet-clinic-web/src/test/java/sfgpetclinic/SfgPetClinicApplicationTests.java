3cd83785782abe301e2b9b6b60977fcb235cf303
bourisdim <bourisdim@outlook.com>
2022-09-22 14:23:32 +0300

    Remove Lombock from dependencies

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,11 +1,12 @@
-package sfgpetclinic;
+package test.java.sfgpetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
+
 @SpringBootTest
 public class SfgPetClinicApplicationTests {
 
