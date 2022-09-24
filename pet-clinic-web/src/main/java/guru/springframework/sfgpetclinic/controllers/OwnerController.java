bf7ebffbb08c5e2951a7d67165c5da476e58195f
bourisdim <bourisdim@outlook.com>
2022-09-24 20:11:32 +0300

    Listing Vets and Owners and their respective cotrollers and views.
    Generating Ids for BaseEntitys

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,15 +1,28 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners", "/owners.html", "owners/index","owners/index.html",""})
-    public String listControllers(){
+    private final OwnerService ownerService;
 
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"/owners", "/owners.html", "owners/index","owners/index.html",""})
+    public String listControllers(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners";
     }
 }
