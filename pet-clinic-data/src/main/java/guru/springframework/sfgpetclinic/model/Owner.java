03a7a41349cdcb781edc5925795fe40d7e005745
bourisdim <bourisdim@outlook.com>
2022-10-07 10:25:20 +0300

    Added SpecialtyService and Map imlementation

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,25 +1,28 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by jt on 7/13/18.
  */
+
+@Entity
+@Table(name="owners")
 public class Owner extends Person {
 
+    @Column
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 
     public String getAddress() {
         return address;
@@ -43,5 +46,35 @@ public class Owner extends Person {
 
     public void setTelephone(String telephone) {
         this.telephone = telephone;
+    }
+    public Owner() {
+    }
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Owner owner = (Owner) o;
+
+        if (address != null ? !address.equals(owner.address) : owner.address != null) return false;
+        if (city != null ? !city.equals(owner.city) : owner.city != null) return false;
+        return telephone != null ? telephone.equals(owner.telephone) : owner.telephone == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = address != null ? address.hashCode() : 0;
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (telephone != null ? telephone.hashCode() : 0);
+        return result;
     }
 }
